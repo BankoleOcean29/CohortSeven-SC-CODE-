94ffa178bcaa9aceebca4df12bb5a19a77e12499
Bankole Babarinsa <bankolescripted@gmail.com>
2021-05-16 02:47:40 +0100

    Update on setting speed for my bike

---
+++ Test/OceanBiketest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+public class OceanBiketest {
+
+    @Test
+
+    public void testThatOceanBikeSpeedCanBeSet() {
+        OceanBike alpha = new OceanBike();
+        OceanBike.setOceanBikeSpeed(0);
+
+    }
+}
---
+++ src/OceanBike.java
@@ -0,0 +1,15 @@
+public class OceanBike {
+    
+    private int SpeedInKmPerHour;
+    private String gear;
+    private String fuelGuage;
+    
+    public static void main(String[] args) {
+        
+        
+    }
+
+    public static void setOceanBikeSpeed(int i) {
+
+    }
+}
